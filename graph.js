86ade0bc1e3ecc0bc25302d44a4cf5ec00be06cf
Kevin Mehall <km@kevinmehall.net>
2009-06-07 20:37:32 -0600

    fix graph renderer: antialiasing bugs, higlighting axis

--- graph.js
+++ graph.js
@@ -24,7 +24,7 @@ function graph(ctx, gp, f, r, g, b, lw){
 	var xpixstep = (xmax-xmin)/(width)
 	var ypixstep = -(ymax-ymin)/(height)
 	
-	var x=0; var y=0; var i=0; var vx=xmin; var vy=ymax;
+	var x=0; var y=0; var i=0; var vx=xmin-xpixstep/2; var vy=ymax+ypixstep/2;
 	var sign=f(vx, vy);
 	
 	while (i<pix.length){
@@ -32,19 +32,21 @@ function graph(ctx, gp, f, r, g, b, lw){
 		pix[i+1] = g;
 		pix[i+2] = b;
 		
-		vx+=xpixstep;
-		var nsign=f(vx, vy);
+		vx+=xpixstep/2;
+		var nsign1=f(vx, vy);
+		vx+=xpixstep/2;
+		var nsign2=f(vx, vy);
 		
-		if (sign!=nsign){
-			for (var wx=vx-xpixstep; wx+=xpixstep/aa; wx<vx) 
+		if (sign!=nsign1 || sign!=nsign2){
+			for (var wx=vx-xpixstep; wx+=xpixstep/aa; wx<=vx) 
 				if (f(wx, vy)!=sign) break;
 				
 			var px=i+3;
 			var offset=lw/2+(wx-vx)/xpixstep
-			if (x+offset>=width) offset=width-x
+			if (x+offset>width) offset=width-x
 			for (; offset>0; offset--){
 				if (offset>=1) pix[px]=255
-				else pix[px]=offset*255
+				else pix[px]=offset*256
 				px+=4
 			}
 			var px=i-1;
@@ -52,40 +54,38 @@ function graph(ctx, gp, f, r, g, b, lw){
 			if (x-offset<0) offset=x
 			for (; offset>0; offset--){
 				if (offset>=1) pix[px]=255;
-				else pix[px]=offset*255;
+				else pix[px]=offset*256;
 				px-=4
 			}
 		}
-		
-		sign=nsign;
+		sign=nsign2;
 		i+=4;
-		x+=1	
-		if (x==width){ x=0; y+=1; vx=xmin; vy+=ypixstep; sign=f(vx, vy)};
+		x+=1
+		if (x==width){ x=0; y+=1; vx=xmin-xpixstep/2; vy+=ypixstep; sign=f(vx, vy)};
 	}
-	var x=0; var y=0; var i=0; var vx=xmin; var vy=ymax;
+	var x=1; var y=0; var i=4; var vx=xmin; var vy=ymax;
 	var sign=f(vx, vy);
 	while (i<pix.length){
-		
 		var nsign=f(vx, vy);
 		
 		if (sign!=nsign){
-			for (var wy=vy-ypixstep; wy+=ypixstep/aa; wy<vy) 
+			for (var wy=vy-ypixstep; wy+=ypixstep/aa; wy<=vy) 
 				if (f(vx, wy)!=sign) break;
 				
-			var py=i+3;
+			var py=i-1;
 			var offset=lw/2+(wy-vy)/ypixstep
 			if (y+offset>height) offset=height-y
 			for (; offset>0; offset--){
 				if (offset>=1) pix[py]=255
-				else pix[py]=Math.max(offset*255, pix[py]);
+				else pix[py]=Math.max(Math.floor(offset*256), pix[py]);
 				py+=stride
 			}
-			var py=i+3-stride;
+			var py=i-1-stride;
 			var offset=lw/2+(vy-wy)/ypixstep
 			if (y-offset<0) offset=y
 			for (; offset>0; offset--){
 				if (offset>=1) pix[py]=255;
-				else pix[py]=Math.max(offset*255, pix[py]);
+				else pix[py]=Math.max(Math.floor(offset*256), pix[py]);
 				py-=stride 
 			}
 		}
@@ -95,7 +95,6 @@ function graph(ctx, gp, f, r, g, b, lw){
 		y+=1	
 		if (y==height){ y=0; x+=1; vy=ymax, vx+=xpixstep; i=x*4; sign=f(vx, vy);};
 	}
-	
 	ctx.putImageData(imgd, 0,0);
 	return canvas;
 }
@@ -109,10 +108,10 @@ function drawGrid(ctx, gp){
 	
 	function x2px(x){ return (x-gp.xmin)/xpixstep }
 	function y2py(y){ return (gp.ymax-y)/ypixstep }
-	ctx.strokeStyle='rgba(0,0,255,0.3)'
+	ctx.strokeStyle='rgba(0,0,255,0.2)'
 	
 	for (var x=Math.round(gp.xmin/xgrid)*xgrid; x<gp.xmax; x+=xgrid){
-		if (x==0) ctx.lineWidth=4
+		if (Math.abs(x)<0.00001) ctx.lineWidth=4
 		else ctx.lineWidth=1
 		ctx.beginPath()
 		ctx.moveTo(x2px(x), y2py(gp.ymax))
@@ -121,7 +120,7 @@ function drawGrid(ctx, gp){
 	}
 	
 	for (var y=Math.round(gp.ymin/ygrid)*ygrid; y<gp.ymax; y+=ygrid){
-		if (y==0) ctx.lineWidth=4
+		if (Math.abs(y)<0.00001) ctx.lineWidth=4
 		else ctx.lineWidth=1
 		ctx.beginPath()
 		ctx.moveTo(x2px(gp.xmin), y2py(y))
